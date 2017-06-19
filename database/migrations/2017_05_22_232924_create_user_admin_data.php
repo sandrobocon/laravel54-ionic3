1383c420cf691a75446772094ea424a4ee7af767
sandro <sandro_bocon@yahoo.com.br>
2017-06-19 20:56:00 -0300

    php artisan make:auth (created files)

---
+++ database/migrations/2017_05_22_232924_create_user_admin_data.php
@@ -0,0 +1,35 @@
+<?php
+
+use CodeFlix\Models\User;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserAdminData extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        User::create([
+            'name' => env('ADMIN_DEFAULT_NAME', 'Administrador'),
+            'email' => env('ADMIN_DEFAULT_EMAIL', 'admin@user.com'),
+            'password' => bcrypt(env('ADMIN_DEFAULT_PASSWORD', 'secret')),
+            'role' => User::ROLE_ADMIN
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $user = User::where('email','=',env('ADMIN_DEFAULT_EMAIL'))->first();
+        if($user instanceof User){
+            $user->delete();
+        }
+    }
+}
