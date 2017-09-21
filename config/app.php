0ba373680870ef1568cde2c8c880a4efa5c33142
sandro <sandro_bocon@yahoo.com.br>
2017-09-20 21:07:43 -0300

    Fresh install "dingo/api" and "tymon/jwt-auth" used to create our API $composer require dingo/api:1.0.0-beta8 $php artisan vendor:publish --provider="Dingo\Api\Provider\LaravelServiceProvider" $composer require tymon/jwt-auth:"dev-develop#9f759fe9e3112e1de50920c05537d752780cf966" Set necessary code in 'config/app.php' Generate jwt-secret (random key) $php artisan jwt:secret

--- config/app.php
+++ config/app.php
@@ -172,6 +172,8 @@ return [
         Prettus\Repository\Providers\RepositoryServiceProvider::class,
         Jrean\UserVerification\UserVerificationServiceProvider::class,
         Folklore\Image\ImageServiceProvider::class,
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+        Dingo\Api\Provider\LaravelServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -240,6 +242,7 @@ return [
         'FormBuilder' => Kris\LaravelFormBuilder\Facades\FormBuilder::class,
         'UserVerification' => Jrean\UserVerification\Facades\UserVerification::class,
         'Image' => \Folklore\Image\Facades\Image::class,
+        'ApiRoute' => \Dingo\Api\Facade\Route::class,
     ],
 
 ];
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+ApiRoute::version('v1',function() {
+    ApiRoute::get('/test', function(){
+        return \CodeFlix\Models\User::paginate();
+    });
+});
